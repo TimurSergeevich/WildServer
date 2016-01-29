f3d38288f3decce4b01ea955220bcb9aff70fbea
timur <myalesyataranenko@gmail.com>
2016-01-29 08:29:51 +0300

    Изменения в Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,26 @@
 package main;
 
+import org.eclipse.jetty.server.Server;
+
 /**
  * Created by timur on 29.01.16.
  */
 public class Main {
     public static void main(String[] args){
-        String s = "Good :)";
-        System.out.println(s);
+
+        Server server = new Server(8080);
+
+        try {
+            server.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            server.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
